65b52b4c7ad3a8fb063cf7a8f3fc63bacfd07626
nomail <no@nomail.no>
2025-07-11 12:25:41 +0200

    Ejemplo de ciclo for y arrays

---
+++ src/main/java/Sumatorio.java
@@ -0,0 +1,33 @@
+public class Sumatorio {
+
+    int s1[];
+
+
+    public void lanzar() {
+        int a = 0;
+
+
+        a = 5;
+
+        s1 = new int[1000];
+
+        for (int i = 0; i < s1.length  ;  i++ ){
+            s1[i] = i +1;
+        }
+
+        int suma = 0;
+        for (int i= 0 ; i < s1.length; i++) {
+            suma += s1[i];
+        }
+
+        int b[] = new int[] { 1, 2, 3, 4, 5, 6, 7}; 
+
+
+        suma = 0;
+        for (int i= s1.length -1 ; i >= 0; i--) {
+            suma += s1[i];
+        }
+
+    }
+
+}
---
+++ src/test/java/SumatorioTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class SumatorioTest {
+    @Test
+    void testLanzar() {
+        Sumatorio sumatorio = new Sumatorio();
+
+        sumatorio.lanzar();
+    }
+}
